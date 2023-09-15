1b16ada826b3348a894a71c724b5aec241ad08fb
Rivalser <97056846+Rivalser@users.noreply.github.com>
2023-09-15 21:23:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,22 +5,24 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     // Nem "NELEMENTS", hanem "N_ELEMENTS" kell.
-    int *b = new int[NELEMENTS];
+    int *b = new int[N_ELEMENTS];
 
     //Nincs lezarva a parancs, a fordito tovobb megy es hibat dob. Hianyzik: ";"
     //Nem karaktert akarunk megadni hanem szöveget ezért "" kell ez helyett: ''
-    std::cout << "1-100 ertekek duplazasa"
+    std::cout << "1-100 ertekek duplazasa";
     //A for nincs befelyezve, kell még a lefutási feltétel, és a léptetési mérték.
-    for (int i = 0;)
+    for (int i = 0; i<N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
     //Az 'i' önmagában csak áll mint egy fa... meg kell adni a feltételt, hogy mi alapján fusson a for pl.: "i<420"
-    for (int i = 0; i; i++)
+    for (int i = 0; i<1; i++)
     {
         //Nincs lezarva a parancs, a fordito tovobb megy es hibat dob. Hianyzik: ";"
         // <<std::endl; kell mert csúnya lesz az eredmény...
-        std::cout << "Ertek:"
+        //Eszétikailag az a szóköz az "Ertek" után nem árt
+        //Mi van ha az előzővel összefésükésre kerülne?
+        std::cout << "Ertek: "<< std::endl;
         //Amúgy nem hiba de minek ez a for?
         //Vagyis hiba mert számítási kapacitást foglal le meg hülyén néz ki.
     }    
@@ -29,13 +31,13 @@ int main()
     //Itt kell egy alapérték pl.: atlag = 0, ha nem akarunk semmit, fontos a kinullázás. Hiszen, a memória kiosztáskor "yxnjiíkyghxweitn2378" is lehet ott.
     //A későbbiekben "atlag += b[i];" valamit hozzáadunk. Tehát, ha nem adunk alap értéket, akkor baj van.
     //hádeazátlagazdouble,mivelnemfeltétlenülegészszám.
-    int atlag;
+    double atlag = 0;
 
     // "...< N_ELEMENTS,..." helyett ez kell: < "...N_ELEMENTS;..."
     for (int i = 0; i < N_ELEMENTS; i++)
     {
         //Nincs lezarva a parancs, a fordito tovobb megy es hibat dob. Hianyzik: ";"
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
